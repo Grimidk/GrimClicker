200352a1216a6b2926f65cb873b43986e6736d5f
Grimidk <thegrimidk@gmail.com>
2024-04-15 16:41:56 -0400

    hotfix

--- grim-clicker/src/modules/app/App.jsx
+++ grim-clicker/src/modules/app/App.jsx
@@ -1,21 +1,14 @@
 import { useState } from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css'
-
-function mainClick() {
-  points = Number(points) + Number(clickLevel) + Number(1);
-}
-
-function mainUpgrade() {
-  clickLevel = Number(clickLevel) + Number(1);
-}
 
 function App() {
   const [points, setPoints] = useState(0)
-  const [clickLevel, setLevel] = useState(0)
-
+  const [clickLevel, setLevel] = useState(1)
   return (
     <>
-      <button type="button" id="mainClick" onClick={() => setPoints((points) => points + clickLevel + 1)}>Points: {points}</button>
+      <button type="button" id="mainClick" onClick={() => setPoints((points) => points + clickLevel)}>Points: {points}</button>
       <button type="button" id="mainUpgrade" onClick={() => setLevel((clickLevel) => clickLevel + 1)}>Level: {clickLevel}</button>
     </>
   )
