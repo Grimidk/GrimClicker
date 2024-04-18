8a942fd5f89792b91d57584c4c74ff7a357dda4b
Grimidk <thegrimidk@gmail.com>
2024-04-18 13:12:36 -0400

    tentimes

--- Front/grim-clicker/src/modules/Landing.jsx
+++ Front/grim-clicker/src/modules/Landing.jsx
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from 'react';
 import Header from './Header';
 import './../App.css';
 
+const MAX_LEVEL_CLICK = 100;
+const MAX_LEVEL_AUTO = 100;
+const MAX_LEVEL_COOLDOWN = 20;
+
 function Landing() {
   const [points, setPoints] = useState(0);
   const [clickLevel, setClickLevel] = useState(1);
@@ -33,16 +37,31 @@ function Landing() {
     }
   };
 
+  const triggerUpgrades = (upgradeFunction, cost, level, maxLevel) => {
+    const remainingUpgrades = Math.min(Math.floor(points / cost), maxLevel - level);
+    if (level == maxLevel){
+      window.alert("Can't upgrade anymore, Maxed out");
+    } else {
+      if (remainingUpgrades <= 0) {
+        return;
+      }
+      const upgradesToTrigger = Math.min(remainingUpgrades, 10);
+      for (let i = 0; i < upgradesToTrigger; i++) {
+        upgradeFunction();
+      };
+    };
+  };
+  
   const clickUpgrade = () => {
-    upgrade(clickLevel, clickUpgradeCost, setClickLevel, 100);
+    triggerUpgrades(() => upgrade(clickLevel, clickUpgradeCost, setClickLevel, MAX_LEVEL_CLICK), clickUpgradeCost, clickLevel, MAX_LEVEL_CLICK);
   };
 
   const autoUpgrade = () => {
-    upgrade(autoLevel, autoUpgradeCost, setAutoLevel, 100);
+    triggerUpgrades(() => upgrade(autoLevel, autoUpgradeCost, setAutoLevel, MAX_LEVEL_AUTO), autoUpgradeCost, autoLevel, MAX_LEVEL_AUTO);
   };
 
   const cooldownUpgrade = () => {
-    upgrade(cooldownLevel, cooldownUpgradeCost, setCooldownLevel, 20);
+    triggerUpgrades(() => upgrade(cooldownLevel, cooldownUpgradeCost, setCooldownLevel, MAX_LEVEL_COOLDOWN), cooldownUpgradeCost, cooldownLevel, MAX_LEVEL_COOLDOWN);
   };
 
   useEffect(() => {
@@ -62,12 +81,21 @@ function Landing() {
       <div className="indicator">Points per Second: {pointsPerSecond}</div>
       <button className="button" id="mainClick" onClick={() => setPoints(prevPoints => prevPoints + clickLevel)}>Points: {points}</button>
       <div className='shop'>
-        <button className="button" id="clickUpgrade" onClick={clickUpgrade}>Click Level: {clickLevel}</button>
-        <div className="indicator">Cost: {clickUpgradeCost}</div>
-        <button className="button" id="autoUpgrade" onClick={autoUpgrade}>Auto-Clicker Level: {autoLevel}</button>
-        <div className="indicator">Cost: {autoUpgradeCost}</div>
-        <button className="button" id="cooldownUpgrade" onClick={cooldownUpgrade}>Cooldown: {Math.round(1000 / cooldownLevel)} ms</button>
-        <div className="indicator">Cost: {cooldownUpgradeCost}</div>
+        <div>
+          <button className="button" id="clickUpgrade" onClick={clickUpgrade}>Click Level: {clickLevel}</button>
+          <button className="button" onClick={() => triggerUpgrades(() => upgrade(clickLevel, clickUpgradeCost, setClickLevel, MAX_LEVEL_CLICK), clickUpgradeCost, clickLevel, MAX_LEVEL_CLICK)}>x10</button>
+          <div className="indicator">Cost: {clickUpgradeCost}</div>
+        </div>
+        <div>
+          <button className="button" id="autoUpgrade" onClick={autoUpgrade}>Auto-Clicker Level: {autoLevel}</button>
+          <button className="button" onClick={() => triggerUpgrades(() => upgrade(autoLevel, autoUpgradeCost, setAutoLevel, MAX_LEVEL_AUTO), autoUpgradeCost, autoLevel, MAX_LEVEL_AUTO)}>x10</button>
+          <div className="indicator">Cost: {autoUpgradeCost}</div>
+        </div>
+        <div>
+          <button className="button" id="cooldownUpgrade" onClick={cooldownUpgrade}>Cooldown: {Math.round(1000 / cooldownLevel)} ms</button>
+          <button className="button" onClick={() => triggerUpgrades(() => upgrade(cooldownLevel, cooldownUpgradeCost, setCooldownLevel, MAX_LEVEL_COOLDOWN), cooldownUpgradeCost, cooldownLevel, MAX_LEVEL_COOLDOWN)}>x10</button>
+          <div className="indicator">Cost: {cooldownUpgradeCost}</div>
+        </div>
       </div>
       <div className="settings">
         <button className="button" id="reset" onClick={reset}>Hard Reset</button>
